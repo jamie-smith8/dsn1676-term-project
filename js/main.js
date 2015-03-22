673b758f27543f78c62f9b0e3ad224dcc1381952
Jamie <smit1365@algonquinlive.com>
2015-03-21 20:07:14 -0400

    javascript stuff

--- js/main.js
+++ js/main.js
@@ -4,3 +4,17 @@ var $panel = $('.panel');
 $btn.on('click', function () {
     $panel.toggleClass('js-panel-open');
 });
+
+var $btnSlide = $('.btn-surf');
+var $surf = $('.surf');
+
+$btnSlide.on('click', function () {
+    $surf.toggleClass('js-slide');
+});
+
+var $btnFade = $('.btn-fade');
+var $hula = $('.hula');
+
+$btnFade.on('click' function () {
+    $hula.toggleClass('js-fade');
+}
